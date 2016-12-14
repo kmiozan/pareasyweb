a2f54ec38a70e602b0faaf43f3a7009e38bf2db0
ozantoteles <ihsanozan.yildirim@arcelik.com>
2016-12-14 17:09:28 +0300

    Django Forms implemented (except security part)

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.postList, name='postList'),
     url(r'^post/(?P<pk>\d+)/$', views.postDetail, name='postDetail'),
+    url(r'^post/new/$', views.postNew, name='postNew'),
+    url(r'^post/(?P<pk>\d+)edit/$', views.postEdit, name='postEdit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -15,3 +16,37 @@ def postDetail(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/postDetail.html', {'post': post})
+
+def postNew(request):
+    """  """
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publishedDate = timezone.now()
+            post.save()
+            return redirect('postDetail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/postEdit.html', {'form': form})
+
+def postEdit(request, pk):
+    """  """
+
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post. author = request.user
+            post.publishedDate = timezone.now()
+            post.save()
+            return redirect('postDetail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/postEdit.html', {'form': form})
